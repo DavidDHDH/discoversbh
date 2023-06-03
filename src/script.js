537d53abd7bcf5750111e723018be6e1bac62612
DavidDHDH <david.heimfert@gmail.com>
2023-06-03 15:54:35 -0400

    remise à 0 et reconfig du projet avec nouvelles dépendances

--- src/script.js
+++ src/script.js
@@ -1,22 +1,25 @@
 // Affichage de l'heure
+function checkTime(i) {
+  if (i < 10) {
+    i = `0${i}`;
+  } // add zero in front of numbers < 10
+  return i;
+}
 
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    m = checkTime(m);
-    document.getElementById("time").innerHTML =
-    +h + ":" + m;
-    var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-   
+  const today = new Date();
+  const h = today.getHours();
+  let m = today.getMinutes();
+  // const s = today.getSeconds();
+  m = checkTime(m);
+  // s = checkTime(s);
+  document.getElementById("time").innerHTML = `${h}:${m}`;
+  setTimeout(startTime, 500);
+}
+
 startTime();
 
-/*CONTACT ME
+/* CONTACT ME
 Envoi un email à moi meme avec adresse email du contact :
 - recuperer le champ email
 - l'integre dans un email vierge
@@ -37,10 +40,6 @@ function sendEmail(){
     message => alert(message)
   )}
 
-
+  https://lawaltech.hashnode.dev/comment-envoyer-un-e-mail-en-utilisant-js-sans-backend
   message = "Thanks, we will get back to you as soon as possible"
   */
-
-
-
-
